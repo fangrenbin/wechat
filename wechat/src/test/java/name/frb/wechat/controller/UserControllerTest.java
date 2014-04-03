fb450798e58cdc0098005e540867358354485a1f
fangrenbin <fangrenbin@gmail.com>
2014-04-03 17:19:44 +0800

    Fix wechat-template.xml

--- wechat/src/test/java/name/frb/wechat/controller/UserControllerTest.java
+++ wechat/src/test/java/name/frb/wechat/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package name.frb.wechat.controller;
 
+import name.frb.configuration.resourceloader.RescourceLoader;
 import name.frb.configuration.xmlconfiguration.XmlConfiguration;
 import name.frb.wechat.AbstractTestng;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,30 @@ import java.util.Date;
 
 public class UserControllerTest extends AbstractTestng {
 //    @Autowired
-    private XmlConfiguration wechatTemplate;
+//    private XmlConfiguration wechatTemplate;
 
 //    @Test
-    public void readWechatTeplateTest() {
+//    public void readWechatTeplateTest() {
+//
+//    }
 
+    public static void main(String[] args) {
+        RescourceLoader rescourceLoader = new RescourceLoader();
+        XmlConfiguration wechatTemplate = new XmlConfiguration("/Users/renbinfang/Programming/github.com/wechat-klxenglish/wechat/src/main/resources/wechat/wechat-template.xml");
+        wechatTemplate.setRescourceLoader(rescourceLoader);
+        wechatTemplate.init();
+
+        String replyContent = "感谢关注快乐学ENGLISH微信号，本号码正在开发当中，希望您耐心等待！";
+
+        String replayMessage = wechatTemplate.getString("TextMessage")
+                .replace("${ToUserName}", "<![CDATA[" + "o8vWct1kjxAViVf2u_BZHuR4bbeU" + "]]>")
+                .replace("${FromUserName}", "<![CDATA[" + "gh_583174c5755e" + "]]>")
+                .replace("${CreateTime}", String.valueOf(new Date().getTime()))
+                .replace("${MsgType}", "<![CDATA[" + "text" + "]]>")
+                .replace("${Content}", "<![CDATA[" + replyContent + "]]>");
+
+        System.out.println(replayMessage);
     }
-
 
     public static String sha1(String key) {
         try {
