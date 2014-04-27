eaf6914d6e6b9a19e69a34fe908b69d2c6d1dbcc
fangrenbin <fangrenbin@gmail.com>
2014-04-27 11:17:45 +0800

    add webpage resources.

---
+++ wechat-manager/src/main/java/name/frb/wechat/manager/vo/UserMessageListVo.java
@@ -0,0 +1,53 @@
+package name.frb.wechat.manager.vo;
+
+/**
+ * Created by 斌 on 2014/4/27.
+ */
+public class UserMessageListVo {
+    private String toUserName;
+    private String fromUserName;
+    private String createTime;
+    private String content;
+
+    public String getToUserName() {
+        return toUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        this.toUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "UserMessageListVo{" +
+                "toUserName='" + toUserName + '\'' +
+                ", fromUserName='" + fromUserName + '\'' +
+                ", createTime='" + createTime + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
